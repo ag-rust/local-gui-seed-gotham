4e48822298a9405d8b02cf3f999d5c9e10d7a99c
Florian Warzecha <liketechnik@disroot.org>
2020-02-07 00:13:39 +0100

    serve the gui on server side

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,13 @@
-pub fn main() -> () {
+use gotham::handler::assets::FileOptions;
+use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
 
+pub fn main() {
+    let addr = "127.0.0.1:8080";
+
+    let router = build_simple_router(|route| {
+        route.get("gui/pkg/*").to_dir(FileOptions::new("./gui/pkg"));
+        route.get("/").to_file(FileOptions::new("./gui/index.html"));
+    });
+
+    gotham::start(addr, router)
 }
