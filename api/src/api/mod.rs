94186669047b55189666f4fd5890b802b1d9dfff
Florian Warzecha <liketechnik@disroot.org>
2020-02-29 15:02:37 +0100

    api endpoint to terminate server

--- api/src/api/mod.rs
+++ api/src/api/mod.rs
@@ -1,1 +1,8 @@
 pub mod counter;
+
+use gotham::state::State;
+use hyper::{Body, Response};
+
+pub fn terminate(_state: State) -> (State, Response<Body>) {
+    std::process::exit(0);
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -8,14 +8,14 @@ mod data;
 
 use data::counter::CounterState;
 
-use crate::api::counter::{
-    get_counter, post_counter_decrement, post_counter_increment, post_counter_init,
+use crate::api::{
+    counter::{get_counter, post_counter_decrement, post_counter_increment, post_counter_init},
+    terminate,
 };
 use gotham::handler::assets::FileOptions;
 use gotham::middleware::state::StateMiddleware;
 use gotham::pipeline::{single::single_pipeline, single_middleware};
 use gotham::router::builder::{build_router, DefineSingleRoute, DrawRoutes};
-use gotham::router::Router;
 
 pub fn main() {
     let addr = "127.0.0.1:8080";
@@ -26,6 +26,7 @@ pub fn main() {
     let (chain, pipelines) = single_pipeline(pipeline);
 
     let router = build_router(chain, pipelines, |route| {
+        route.post("api/v1/terminate").to(terminate);
         route.get("api/v1/counter").to(get_counter);
         route.post("api/v1/counter/init").to(post_counter_init);
         route
