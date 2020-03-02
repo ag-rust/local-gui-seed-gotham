6ffc3751b04a55ca93e7dc64e8e733894dec658f
Florian Warzecha <liketechnik@disroot.org>
2020-03-02 20:20:59 +0100

    crossplatform comapatability for asset preparation (#1)
    rewrite prepare_assets.sh inline in duckscript so it also works on
    windows

--- prepare_assets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -d gui/assets ]; then
-	mkdir gui/assets
-fi
-
-cp gui/pkg/gui.js gui/assets
-cp gui/pkg/gui_bg.wasm gui/assets
-cp gui/index.html gui/assets
-cp gui/wing.min.css gui/assets
